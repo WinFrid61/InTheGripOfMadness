ecbb73f5a390107f5f8642db08a2535e2cb1e0db
WinFrid61 <artem.shadrin@mail.ru>
2022-06-23 18:12:36 +0300

    Обновление сцен
    Забыл сохранить весь добавленный материал. Заливаю сейчас.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using CodeMonkey.Utils;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //private void vector3 startingposition;
+
+    //    private void start()
+    //{
+    //    startingposition = transform.position;
+    //}
+
+    //private vector3 getroamingposition;
+    //{
+
+    //}
+}
